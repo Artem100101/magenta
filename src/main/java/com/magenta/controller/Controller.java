79bb16a87df2eecfc5eebb94e378bb0a31e2b01b
Artem100101 <artemufimtsev01@mail.ru>
2023-11-05 23:43:17 +0400

    .

--- src/main/java/com/magenta/controller/Controller.java
+++ src/main/java/com/magenta/controller/Controller.java
@@ -23,17 +23,20 @@ public class Controller {
     }
 
     @PostMapping("/cities")
+//    С ПОМОЩЬЮ ДАННОГО ЗАПРОСА СОЗДАЕТСЯ ОБЪЕКТ В ТАБЛИЦЕ CITY
     public int createCity(@RequestBody CityRequest cityRequest){
         return cityService.createCity(cityRequest.getName(),
                 cityRequest.getLatitude(), cityRequest.getLongitude());
     }
 
     @GetMapping("/cities/{id}")
+//    С ПОМОЩЬЮ ДАННОГО ЗАПРОСА МОЖНО ПОЛУЧИТЬ ПО ID КОНКРЕТНЫЙ ГОРОД
     public CityResponse getCity(@PathVariable Long id){
         return new CityResponse(cityService.getCityById(id));
     }
 
     @GetMapping("/cities")
+    //    С ПОМОЩЬЮ ДАННОГО ЗАПРОСА МОЖНО ПОЛУЧИТЬ СПИСОК ВСЕХ ГОРОДОВ И ТАБЛИЦЫ CITY
     public List<CityResponse> getAllCities(){
         return cityService.getAll().stream().map(CityResponse::new)
                 .collect(Collectors.toList());
@@ -42,6 +45,7 @@ public class Controller {
 
 
     @PostMapping("/distances/crowFlight")
+    //    С ПОМОЩЬЮ ДАННОГО ЗАПРОСА РАССЧИТЫВАЕТСЯ РАССТОЯНИЕ МЕЖДУ ГОРОДАМИ С ИСПОЛЬЗОВАНИЕМ МЕТОДА crowFlight
     public Double createDistance(@RequestParam String fromCity, String toCity){
         CityResponse city1 = new CityResponse(cityService.getCityByName(fromCity));
         CityResponse city2 = new CityResponse(cityService.getCityByName(toCity));
@@ -49,17 +53,20 @@ public class Controller {
     }
 
     @GetMapping("/distances/{id}")
+    //    С ПОМОЩЬЮ ДАННОГО ЗАПРОСА МОЖНО ПОЛУЧИТЬ ПО ID КОНКРЕТНУЮ ДИСТАНЦИЮ
     public DistanceResponse getDistance(@PathVariable Long id){
         return new DistanceResponse(distanceService.getDistanceById(id));
     }
 
     @GetMapping("/distances")
+    //    С ПОМОЩЬЮ ДАННОГО ЗАПРОСА МОЖНО ПОЛУЧИТЬ СПИСОК ВСЕХ ДИСТАНЦИЙ И ТАБЛИЦЫ DISTANCE
     public List<DistanceResponse> getAll(){
         return distanceService.getAll().stream().map(DistanceResponse::new)
                 .collect(Collectors.toList());
     }
 
     @PostMapping("/distances/matrix")
+    //    С ПОМОЩЬЮ ДАННОГО ЗАПРОСА РАССЧИТЫВАЕТСЯ РАССТОЯНИЕ МЕЖДУ ГОРОДАМИ С ИСПОЛЬЗОВАНИЕМ МЕТОДА Matrix
     public Double calculateMatrix(@RequestParam String fromCity,@RequestParam String toCity,@RequestParam String[] intermediateСities){
         CityResponse city1 = new CityResponse(cityService.getCityByName(fromCity));
         CityResponse city2 = new CityResponse(cityService.getCityByName(toCity));
